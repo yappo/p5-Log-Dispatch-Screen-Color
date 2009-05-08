fa819b7f394048eb60cacb67485203ee1f05b8f3
大沢 和宏 <yappo>
2009-05-08 14:15:25 +0900

    Checking in changes prior to tagging of version 0.02.  Changelog diff is:

--- lib/Log/Dispatch/Screen/Color.pm
+++ lib/Log/Dispatch/Screen/Color.pm
@@ -2,7 +2,7 @@ package Log::Dispatch::Screen::Color;
 use strict;
 use warnings;
 use base 'Log::Dispatch::Screen';
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use Params::Validate qw(validate HASHREF);
 Params::Validate::validation_options( allow_extra => 1 );
